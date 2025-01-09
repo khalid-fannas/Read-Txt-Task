6684f577d6c791e956e28df7d815d132708cd129
khalid-fannas <kha2000.khaled@gmail.com>
2025-01-09 18:17:42 +0300

    Task solved

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  fs.readFile("msg.txt", "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "content-type": "text/plain" });
+      res.end("server down");
+    } else {
+      res.writeHead(200, { "content-type": "text/plain" });
+      res.end(data);
+    }
+  });
+});
+
+const port = 3000;
+const HOST = "127.0.0.1";
+
+server.listen(port, (host) => {
+  console.log(`server is running on port ${port} and host ${HOST}`);
+});
